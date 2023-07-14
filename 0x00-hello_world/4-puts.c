3128f5dd9331e2b0066db33b04ae49431aba1bcc
George Kiio <mukiux@gmail.com>
2023-07-14 02:46:40 +0000

    Minor Update on C, alx-low_level_programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *  * main - Program that prints a line with puts function
- *   * Return: 0 (Success)
- *    */
+ * main - Program that prints a line with puts function by mukiux
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
