fa5115cd161f91904c90364a9cf6b931261b9c0a
George Kiio <mukiux@gmail.com>
2023-07-18 01:19:16 -0700

    Minor updates, Variables, if, else, while

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * void - empty
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+        {
+                printf("_putchar\n");
+        }
+        return (0);
+}
