9a7cbb81586917ce68ce6d5171c0ded841fddbce
George Kiio <mukiux@gmail.com>
2023-08-29 08:30:54 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t l = 0;
+	unsigned int count = 0;
 
-	while (h)
+	while (h!=NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		++l;
+		++count;
 	}
-	return (l);
+	return (count);
 }
