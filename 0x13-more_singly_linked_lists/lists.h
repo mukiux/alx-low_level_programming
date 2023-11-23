94fb294ab209b15db8db39aa7c74c5a3097b23cf
George Kiio <mukiux1@gmail.com>
2023-11-23 06:47:22 -0800

    0x14. C - Bit manipulation redo

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -12,9 +12,10 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
-} listint_t;
+	int n;
+	struct listint_s *next;
+}
+	listint_t;
 
 /* Function prototype for print_listint */
 size_t print_listint(const listint_t *h);
